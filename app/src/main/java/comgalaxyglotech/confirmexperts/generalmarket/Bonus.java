22a67a26316618fc895c8f7a1b883292a5fc0feb
DESKTOP-5AP204E\ZYNXX <oluyinka.akinatyo@gmail.com>
2021-01-05 05:48:35 +0100

    bonus page implem

--- app/src/main/java/comgalaxyglotech/confirmexperts/generalmarket/Bonus.java
+++ app/src/main/java/comgalaxyglotech/confirmexperts/generalmarket/Bonus.java
@@ -2,13 +2,42 @@ package comgalaxyglotech.confirmexperts.generalmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Bonus extends AppCompatActivity {
-
+    private pl.droidsonroids.gif.GifImageView claimBtn;
+    private LinearLayout claimPage,oopsPage;
+    private ImageView cancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bonus);
         TransitionAdvertDisplay.StopThisActivity.finish();
+      setup();
+      clickListener();
+    }
+    private void setup(){
+        claimBtn = findViewById(R.id.claimBtn);
+        oopsPage = findViewById(R.id.oopsPage);
+        claimPage = findViewById(R.id.claimPage);
+        cancel = findViewById(R.id.cancel);
+    }
+    private void clickListener(){
+        claimBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                oopsPage.setVisibility(View.VISIBLE);
+                claimPage.setVisibility(View.GONE);
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                claimPage.setVisibility(View.VISIBLE);
+                oopsPage.setVisibility(View.GONE);
+            }
+        });
     }
 }
