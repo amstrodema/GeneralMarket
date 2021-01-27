9aa40c2447ef90cef768efbd1670ed3494ceaa4c
DESKTOP-5AP204E\ZYNXX <oluyinka.akinatyo@gmail.com>
2021-01-27 21:03:15 +0100

    creating setup for setup

--- app/src/main/java/comgalaxyglotech/confirmexperts/generalmarket/DbHelper.java
+++ app/src/main/java/comgalaxyglotech/confirmexperts/generalmarket/DbHelper.java
@@ -1,23 +1,80 @@
 package comgalaxyglotech.confirmexperts.generalmarket;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
 public class DbHelper extends SQLiteOpenHelper {
+    private ModelClass modelClass = new ModelClass();
     public DbHelper(Context context) {
-        super(context, "myMarketDb.db", null, 1);
+        super(context, "MarketDb.db", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase DB) {
-        DB.execSQL("create Table UserDetails(id TEXT primary key, userID TEXT, fName TEXT, lName TEXT, email TEXT, phone TEXT, isMarketCleaning BIT, isAppForceUpdate BIT, storeCost DOUBLE, transactionFee FLOAT, lastUpdateDate TEXT)");
+        DB.execSQL("create Table appSettings( settingName TEXT, valueText TEXT, valueInt INT, valueDouble Double, dataType TEXT)");
+        DB.execSQL("create Table userDetails( settingName TEXT, valueText TEXT, valueInt INT, valueDouble Double, dataType TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
-
+        DB.execSQL("drop Table if exists appSettings");
+        DB.execSQL("drop Table if exists userDetails");
+    }
+    public boolean insertUserDetails(ArrayList<SettingsModel> userDetails){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        DB.delete("userDetails",null,  null);
+        boolean isDone = true;
+        for (SettingsModel x: userDetails) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("settingName",x.getSettingName());
+            if(!x.getValueText().isEmpty())
+                contentValues.put("valueText",x.getValueText());
+            else if(x.getValueDouble() != 0)
+                contentValues.put("valueInt",x.getValueDouble());
+            else if(x.getValueInt() != 0)
+                contentValues.put("valueDouble",x.getValueInt());
+           long result = DB.insert("userDetails", null, contentValues);
+           if(result == -1)
+               isDone =false;
+        }
+        return isDone;
+    }
+    public boolean insertAppSettings(ArrayList<SettingsModel> userDetails){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        DB.delete("appSettings",null,  null);
+        boolean isDone = true;
+        for (SettingsModel x: userDetails) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("settingName",x.getSettingName());
+            if(!x.getValueText().isEmpty())
+                contentValues.put("valueText",x.getValueText());
+            else if(x.getValueDouble() != 0)
+                contentValues.put("valueInt",x.getValueDouble());
+            else if(x.getValueInt() != 0)
+                contentValues.put("valueDouble",x.getValueInt());
+            long result = DB.insert("appSettings", null, contentValues);
+            if(result == -1)
+                isDone =false;
+        }
+        return isDone;
+    }
+    public Cursor getUserData(){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from userDetails", null);
+        cursor.close();
+        return cursor;
+    }
+    public Cursor getAppSettings(){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("Select * from appSettings", null);
+        cursor.close();
+        return cursor;
     }
 }
